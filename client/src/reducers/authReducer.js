d93ff054195f98f07305bc0d7739ba7dcbab43cf
Mario Zamora <maza1387@gmail.com>
2017-08-14 18:19:35 -0700

    Develop Client Side

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,18 @@
+export default function(state = {}, action) {
+  switch (action.type) {
+    // case expression:
+    //
+    //   break;
+    // case expression:
+    //
+    //   break;
+    // case expression:
+    //
+    //   break;
+    // case expression:
+    //
+    //   break;
+    default:
+      return state;
+  }
+}

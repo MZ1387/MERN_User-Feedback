26f564802afba1d3dd39986a441552e3fb671dd4
Mario Zamora <mariozamora@marios-mbp-2.attlocal.net>
2017-10-28 09:32:25 -0700

    Create React Enviornment Variable, Create Stripe Component, Create Stripe Action, Create Stripe API Route

--- services/passport.js
+++ services/passport.js
@@ -25,12 +25,12 @@ passport.use(
   },
   async (accessToken, refreshToken, profile, done) => {
     const existingUser = await User.findOne({ googleId: profile.id })
-    
-      if (existingUser) {
-        return done(null, existingUser);
-      }
 
-      const user = await new User({ googleId: profile.id }).save()
-      done(null, user);
+    if (existingUser) {
+      return done(null, existingUser);
+    }
+
+    const user = await new User({ googleId: profile.id }).save()
+    done(null, user);
   })
 );

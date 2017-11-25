a647678037e9aebb444b2ae230af268e839a8230
Mario Zamora <mariozamora@marios-mbp-2.attlocal.net>
2017-11-25 11:32:01 -0800

    Refactor Form Fields

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 const SurveyReview = ({ onCancel, formValues: { title, subject, body, emails } }) => {
   return (

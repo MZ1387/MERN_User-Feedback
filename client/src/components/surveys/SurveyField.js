a53a444d5514cb339ac4305d2eb2a02b7ecddeb2
Mario Zamora <mariozamora@marios-mbp-2.attlocal.net>
2017-11-24 21:05:46 -0800

    Create Custom Field Component

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class SurveyField extends Component {
+  render() {
+    return (
+      <div>
+        <input />
+      </div>
+    );
+  }
+}
+
+export default SurveyField;
+
+// <Field
+//   type="text"
+//   name="surveyTitle"
+//   component="input"
+// />
